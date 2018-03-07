fe2c1708034af7cc404edb8d68ad5a6602535cfc
Kasper Meerts <kaspermeerts@gmail.com>
2018-03-07 18:31:48 +0100

    Updated coverage script and readme

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
-# Super Mario Land disassembly
+# Super Mario Land Disassembly
 
 Disassembly of the first Game Boy game I ever played
 
 This repository builds Super Mario Land (World) (Rev A) with SHA1 checksum `418203621b887caa090215d97e3f509b79affd3e`
 
-As of now it requires a copy of the original ROM named "baserom.gb" to be placed in the repository
+As of now it requires a copy of the original ROM named "baserom.gb" to be placed in the repository, to fill in sections which have not been disassembled yet. The goal is to make this step obsolete.
 
 Requires RGBDS to build
+
+## Coverage
+
+A quick and dirty Python script `coverage.py` is provided to estimate how much of the ROM has been disassembled
+
+* Bank 0: about a quarter
+* Bank 1-3: nothing yet
+* High RAM: 26 out of 127 bytes identified
+
--- coverage.py
+++ coverage.py
@@ -3,6 +3,13 @@ import os
 
 BANKS = 4
 BANK_SIZE = 1 << 16 >> 2
+ROM_SIZE = BANK_SIZE * BANKS
+
+# Funny how the Game Boy had to make do with 8 kB of RAM, and I can now use a 
+# array many times that size without thinking about it. It's not even worth it
+# at all to use a more efficient approach
+coverage_map = [1] * ROM_SIZE
+
 # Subtract what is included from the binary file
 coverage = [BANK_SIZE] * BANKS
 
@@ -27,6 +34,9 @@ def tally_file(filepath):
         else:
             raise
 
+        for i in range(length):
+            coverage_map[start + i] = 0
+
         bank = start // BANK_SIZE
         coverage[bank] -= length
 
@@ -41,3 +51,72 @@ for bank in range(BANKS):
 
 total_coverage = sum([c for c in coverage])
 print("Total : {:5} bytes: {:.3g}%".format(total_coverage, 100 * total_coverage / (BANKS * BANK_SIZE)))
+
+from PIL import Image
+
+WIDTH = 64
+HEIGHT = 64
+KB_HEIGHT = 0x1000 // (WIDTH * 16)
+BANK_HEIGHT = BANK_SIZE // (WIDTH * 16)
+KB_MARGIN = 1
+BANK_MARGIN = 2
+assert WIDTH * HEIGHT * 16 == ROM_SIZE # 16 bytes per tile
+SCALE = 4
+
+PX_WIDTH = WIDTH * 8
+PX_HEIGHT = HEIGHT * 8 + BANK_MARGIN * (BANKS - 1) + KB_MARGIN * (ROM_SIZE // 0x1000 - 1)
+
+PALETTE1 = [(255,255,255), (160,160,160), (85,85,85), (0,0,0)]
+PALETTE2 = [(155,188,15), (139,172,15), (48,98,48), (15,56,16)]
+
+img = Image.new("P", (PX_WIDTH, PX_HEIGHT), 8)
+img.putpalette([
+    255,255,255, 160,160,160, 85,85,85, 0,0,0,      # Monochrome
+    155,188,15, 139,172,15, 48,98,48, 15,56,16,     # Game Boy palette
+    255,255,200
+])
+
+with open("baserom.gb", "rb") as f:
+    rom = f.read()
+
+def copy_tile(ix, iy, covered, bank, kb):
+    tile = rom[(iy*WIDTH + ix) * 16:(iy*WIDTH + ix) * 16 + 16]
+
+    for row in range(8):
+        hi_byte = tile[row * 2]
+        lo_byte = tile[row * 2 + 1]
+
+        y = iy * 8 + row + bank * BANK_MARGIN + kb * KB_MARGIN
+
+        x = ix * 8 + 0
+        index = ((hi_byte & 0b10000000) >> 6) + ((lo_byte & 0b10000000) >> 7)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 1
+        index = ((hi_byte & 0b01000000) >> 5) + ((lo_byte & 0b01000000) >> 6)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 2
+        index = ((hi_byte & 0b00100000) >> 4) + ((lo_byte & 0b00100000) >> 5)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 3
+        index = ((hi_byte & 0b00010000) >> 3) + ((lo_byte & 0b00010000) >> 4)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 4
+        index = ((hi_byte & 0b00001000) >> 2) + ((lo_byte & 0b00001000) >> 3)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 5
+        index = ((hi_byte & 0b00000100) >> 1) + ((lo_byte & 0b00000100) >> 2)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 6
+        index = ((hi_byte & 0b00000010) >> 0) + ((lo_byte & 0b00000010) >> 1)
+        img.putpixel((x,y), covered + index)
+        x = ix * 8 + 7
+        index = ((hi_byte & 0b00000001) << 1) + ((lo_byte & 0b00000001) >> 0)
+        img.putpixel((x,y), covered + index)
+
+
+for ix in range(WIDTH):
+    for iy in range(HEIGHT):
+        covered = all(coverage_map[(iy * WIDTH + ix)*16: (iy * WIDTH + ix)*16 + 16]) # any or all?
+        copy_tile(ix, iy, 4 if covered else 0, iy // BANK_HEIGHT, iy // KB_HEIGHT)
+
+img.save("coverage_map.png")
