f1014c2da2b2f636c9383edfb2d842143d9ded13
Kasper Meerts <kaspermeerts@gmail.com>
2018-03-23 21:07:34 +0100

    Named most enemies

---
+++ enemies.asm
@@ -0,0 +1,105 @@
+; Auto increment?
+Const: MACRO
+\2 EQU \1
+ENDM
+
+	Const $00, CHIBIBO
+	Const $01, CHIBIBO_STOMPED
+	Const $02, PAKKUN_FLOWER
+	Const $03, GANCHAN_SPAWN
+	Const $04, NOKOBON
+	Const $05, NOKOBON_BOMB
+	Const $06, GENKOTSU
+;	Const $07 spawns LIFT
+	Const $08, KING_TOTOMESU
+	Const $09, POMPON_FLOWER
+	Const $0A, HORIZONTAL_PLATFORM
+	Const $0B, VERTICAL_PLATFORM
+	Const $0C, FALLING_SLAB
+;	Const $0D, upside down corpse?
+	Const $0E, FLY ; TODO Also Kumo?
+	Const $0F, FLY_STOMPED
+	Const $10, HONEN
+;	Const $11 chibibo upside down death animation?
+;	Const $12 nokobon upside down death animation
+	Const $13, LIFT
+	Const $14, LIFT_ASCENDING
+;	Const $15 fly death sfx and animation
+	Const $16, MEKABON
+	Const $17, MEKABON_HEAD
+	Const $18, MEKABON_BODY
+;	Const $19 same as 15?
+	Const $1A, DRAGONZAMASU
+;	Const $1B
+	Const $1C, MEKABON_STOMPED ; also SUU_STOMPED
+	Const $1D, YURARIN
+	Const $1E, FIRE_BREATH
+;	Const $1F
+	Const $20, GUNION
+	Const $21, GUNION_EXPLOSION
+	Const $22, GUNION_FIREBALL
+	Const $23, FIREBALL
+	Const $24, YURARIN_BOO
+	Const $25, SUU
+;	Const $26 two byte script to immediately disable oneself
+	Const $27, EXPLOSION
+	Const $28, MUSHROOM_IN_FLIGHT
+	Const $29, MUSHROOM
+	Const $2A, HEART_IN_FLIGHT
+	Const $2B, HEART
+;	Const $2C
+	Const $2D, FLOWER_GROWING
+	Const $2E, FLOWER
+;	Const $2F, TORION_LAUNCHER?
+	Const $30, TORION
+	Const $31, TOKOTOKO
+	Const $32, HIYOIHOI
+;	Const $33 turns into a ganchan
+	Const $34, STAR
+	Const $35, FALLING_SPIKE
+	Const $36, DROP_BLOCK
+	Const $37, DROP_BLOCK_FALLING
+	Const $38, DIAGONAL_PLATFORM_NE
+	Const $39, DIAGONAL_PLATFORM_NW
+	Const $3A, SMALL_VERTICAL_PLATFORM
+	Const $3B, SMALL_HORIZONTAL_PLATFORM
+	Const $3C, BATADON
+	Const $3D, BATADON_STOMPED
+;	Const $3E
+	Const $3F, GAO					; and NYOLOLIN
+	Const $40, GAO_STOMPED			; and TOKOTOKO
+;	Const $41
+	Const $42, BUNBUN
+	Const $43, BUNBUN_STOMPED
+;	Const $44
+	Const $45, ARROW
+;	Const $46
+	Const $47, GANCHAN
+	Const $48, TAMAO
+	Const $49, PIPE_CANNON
+;	Const $4A orients GIRA?
+	Const $4B, GIRA
+;	Const $4C
+;	Const $4D
+;	Const $4E
+;	Const $4F
+;	Const $50
+	Const $51, POMPON_SPORE
+	Const $52, ROKETON
+	Const $53, CHICKEN
+	Const $54, ROTO_DISC
+	Const $55, REVERSE_PAKKUN_FLOWER
+	Const $56, PIONPI
+	Const $57, PIONPI_STOMED
+;	Const $58, DRAGONZAMASU_FIRE?
+	Const $59, CHIKAKO
+	Const $5A, DIAGONAL_GIRA
+;	Const $5B tatanga rising up?
+	Const $5C, CANNONBALL
+	Const $5D, SMALL_CANNONBALL_TOP
+	Const $5E, SMALL_CANNONBALL_MIDDLE
+	Const $5F, SMALL_CANNONBALL_BOTTOM
+	Const $60, TATANGA
+	Const $61, BIOKINTON
+;	Const $62 positions Tatanga?
+
